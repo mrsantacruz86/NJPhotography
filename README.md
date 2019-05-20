a8939fa1e4fd5cd99c9a82f011cc33f0d2c6ea36
Nelson Diaz <abnerdc86@gmail.com>
2019-05-20 00:46:13 -0400

    Uploading initial template

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # njphotography
 Professional photography website.
+
+## Installing dependencies
+
+After cloning the repo use the `npm install` or `yarn install` to download the dependencies for this project.
+
+## Deployment
+
+Create a branch in github called `gh-pages`.
+Remove the `dist` directory from the project’s `.gitignore` file.
+
+Generate the production distribution by excecuting the script: 
+```sh
+yarn build
+```
+or 
+```sh
+npm run build
+```
+
+Make sure git knows about your subtree (the subfolder with your site).
+
+```sh
+git add dist && git commit -m "Initial dist subtree commit"
+```
+Use subtree push to send it to the `gh-pages` branch on GitHub.
+
+```sh
+git subtree push --prefix dist origin gh-pages
+```
---
+++ src/js/app.js
@@ -0,0 +1,45 @@
+import '../css/main.scss';
+import jquery from 'jquery';
+window.$ = jquery;
+import { getGif } from './api';
+import "bootstrap";
+
+// jQuery methods
+$(function () {
+  // handle nav-link active
+  $('.nav li').filter(function () { return this.href == location.href }).parent().addClass('active').siblings().removeClass('active')
+  $('.nav li').click(function () {
+    $(this).parent().addClass('active').siblings().removeClass('active')
+  })
+
+  //add year to the copyright
+  $(function () { $("#crYear").text(new Date().getFullYear()) });
+
+  //handle button click event
+  $("#alert-btn").on("click", () => {
+    getGif("dog")
+      .then(res => {
+        const src = res.data.images.fixed_height.url;
+        // console.log(res.data.images.fixed_height.url);
+        const $imgDisplay = $('<div class="img-display">');
+        $imgDisplay.append($(`<img src="${src}">`))
+        $("#root").append($imgDisplay);
+      })
+      .catch(err => console.log(err));
+  })
+});
+
+
+
+// Script to load the images
+function loadImages() {
+  var tag = "";
+  var i;
+  for (i = 1; i < 67; i++) {
+    tag += '<img src="gallery/img-' + i + '.jpg">';
+  }
+  return tag
+}
+
+// document.getElementById("gallery-1").innerHTML = loadImages();
+
